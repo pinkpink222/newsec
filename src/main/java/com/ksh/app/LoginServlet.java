7bbe2830d66f911689e686c4f4c27817cd103318
pinkpink222 <tkdgurtm100@gmail.com>
2025-07-24 00:23:41 +0900

    123

--- src/main/java/com/ksh/app/LoginServlet.java
+++ src/main/java/com/ksh/app/LoginServlet.java
@@ -3,11 +3,11 @@ package com.ksh.app;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
@@ -18,9 +18,9 @@ public class LoginServlet extends HttpServlet {
         // --- 취약한 SQL Injection 코드 ---
         String sql = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
 
-        String dbUrl = "jdbc:mysql://localhost:3036/mydb";
-        String dbUser = "myappuser";
-        String dbPass = "myappuser123"; // <-- 여기를 실제 비밀번호로 바꿔주세요!
+        String dbUrl = "jdbc:mysql://localhost:3306/mydb";
+        String dbUser = "root";
+        String dbPass = "rocky"; // <-- 여기를 실제 비밀번호로 바꿔주세요!
 
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
